1a761c909fec4846ed235768d1959f866bbbe622
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2025-11-26 12:33:39 +0000

    understood concepts of basic routing and use of middlewares

--- WebServer/dist/index.js
+++ WebServer/dist/index.js
@@ -1,10 +1,33 @@
 import express from "express";
+import { config } from "./config.js";
 const app = express();
 const PORT = 8080;
-app.use("/app", express.static("./src/app"));
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        if (res.statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
+function middlewareMetricsInc(req, res, next) {
+    config.fileserverHits++;
+    next();
+}
+app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/healthz", (req, res) => {
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
+});
+app.get("/metrics", (req, res) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(`Hits: ${config.fileserverHits}`);
+});
+app.get("/reset", (req, res) => {
+    config.fileserverHits = 0;
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("Hits reset to 0");
 });
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/`);
--- WebServer/src/index.ts
+++ WebServer/src/index.ts
@@ -1,13 +1,41 @@
 import express from "express";
-import { Request, Response } from "express";
+import { Request, Response ,NextFunction} from "express";
+import { config } from "./config.js";
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        if (res.statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+  config.fileserverHits++;
+    next();
+}
+
+app.use(middlewareLogResponses);
+
+app.use("/app", middlewareMetricsInc,express.static("./src/app"));
 
 app.get("/healthz", (req: Request, res: Response) => {
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
+});
+
+app.get("/metrics", (req: Request, res: Response) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(`Hits: ${config.fileserverHits}`);
+});
+
+app.get("/reset", (req: Request, res: Response) => {
+    config.fileserverHits = 0;
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("Hits reset to 0");
 });
 
 
